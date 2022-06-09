0bb314f4672cb1d0d83d01b1b1a13305ab348bdd
kacperjagiela <jagielakacper80@gmail.com>
2022-06-10 00:27:14 +0200

    config: minimal configuration with code enforcing

---
+++ README.md
@@ -0,0 +1,3 @@
+# Automated Twitch Clips
+
+### Software that allows to create clips compilation and upload them to youtube
---
+++ clips/.eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["@typescript-eslint"],
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: "module",
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+  },
+  root: true,
+  ignorePatterns: [".eslintrc.js", "node_modules/"],
+  rules: {
+    "linebreak-style": "off",
+    "no-duplicate-imports": "error",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+    "@typescript-eslint/no-non-null-assertion": 0,
+    "@typescript-eslint/explicit-function-return-type": [
+      "error",
+      {
+        allowExpressions: true,
+        allowTypedFunctionExpressions: true,
+        allowHigherOrderFunctions: true,
+        allowDirectConstAssertionInArrowFunctions: true,
+        allowConciseArrowFunctionExpressionsStartingWithVoid: false,
+      },
+    ],
+  },
+};
